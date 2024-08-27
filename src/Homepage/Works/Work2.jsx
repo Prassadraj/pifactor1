9ebb3197b947ec9c8750a304f3c77edac25a6dec
Prassadraj <prassadpr10@gmail.com>
2024-08-27 20:54:00 +0530

    weddingMouseMoving

--- src/Homepage/Works/Work2.jsx
+++ src/Homepage/Works/Work2.jsx
@@ -1,10 +1,16 @@
 import React, { useEffect, useRef } from "react";
-import window from "../../app/images/window.png";
 import Image from "next/image";
 import { Montserrat } from "@next/font/google";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/all";
 import Lenis from "@studio-freight/lenis";
+import Picture1 from "../../../public/images/1.jpeg";
+import Picture2 from "../../../public/images/2.jpeg";
+import Picture3 from "../../../public/images/3.jpg";
+import Picture4 from "../../../public/images/4.jpg";
+import Picture5 from "../../../public/images/5.jpg";
+import Picture6 from "../../../public/images/6.jpg";
+import Picture7 from "../../../public/images/7.jpeg";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -31,7 +37,7 @@ function Work2() {
     requestAnimationFrame(scrollFn);
 
     // Setup GSAP animations with ScrollTrigger
-    // 2danimation text
+    // 2D animation text
     gsap.fromTo(
       ".text p",
       {
@@ -40,13 +46,13 @@ function Work2() {
       {
         y: 0,
         scrollTrigger: ".text",
-
         duration: 0.5,
         ease: "power3.out",
         delay: 0.2,
         stagger: 0.1,
       }
     );
+
     gsap.to(".animations1", {
       height: "50vh",
       width: "100%",
@@ -55,7 +61,6 @@ function Work2() {
       scrollTrigger: {
         trigger: ".animations1",
         start: "top 80%",
-
         end: "bottom 80%",
         scrub: 2,
       },
@@ -94,7 +99,6 @@ function Work2() {
       duration: 1,
       scrollTrigger: {
         trigger: ".animations4",
-
         start: "top 10%",
         end: "bottom 40%",
         scrub: 2,
@@ -108,11 +112,13 @@ function Work2() {
     };
   }, []);
 
+  const pictures = [Picture1, Picture5, Picture6, Picture7];
+
   return (
     <div ref={containerRef} className="md:px-20 md:py-10 w-full h-[240vh]">
       <div
-        className="text flex overflow-hidden "
-        style={{ clipPath: " polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)" }}
+        className="text flex overflow-hidden"
+        style={{ clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)" }}
       >
         {" 2D Animations".split("").map((text, i) => (
           <p
@@ -143,9 +149,11 @@ function Work2() {
                 Dolores, aut eum a, vero, quia.
               </p>
             </div>
-            <div className="img-2d h-full w-full border-white border-2 rounded-md">
+            <div className="img-2d h-full w-full  rounded-md">
               <Image
-                src={window}
+                src={pictures[i].src}
+                width={pictures[i].width}
+                height={pictures[i].height}
                 className="object-cover w-full h-full"
                 alt="Window animation preview"
               />
