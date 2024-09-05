73ba093da0ffb3ef11acf2ecad3fdffd8fe09dfb
Prassadraj <prassadpr10@gmail.com>
2024-09-05 18:31:46 +0530

    menuu

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
       </head>
       <body>
         <Nav />
-        <Menu />
+        {/* <Menu /> */}
         {children}
       </body>
     </html>
