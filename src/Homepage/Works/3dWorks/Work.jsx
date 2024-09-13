3564af97dde62d081739ed300a14fa7fa26c7155
Prassadraj <prassadpr10@gmail.com>
2024-09-14 02:13:58 +0530

    chg

--- src/Homepage/Works/3dWorks/Work.jsx
+++ src/Homepage/Works/3dWorks/Work.jsx
@@ -110,7 +110,7 @@ const Column = ({ images, y }) => {
       {images.map((src, i) => {
         return (
           <div key={i} className={styles.imageContainer}>
-            <Image src={`/3dImages/${src}`} alt="image" fill />
+            <Image priority src={`/3dImages/${src}`} alt="image" fill />
           </div>
         );
       })}
