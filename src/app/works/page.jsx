986efbb3574b25b8fcb8aea98e2b7f67785fda62
Prassadraj <prassadpr10@gmail.com>
2024-09-14 22:57:16 +0530

    chg

--- src/app/works/page.jsx
+++ src/app/works/page.jsx
@@ -4,6 +4,7 @@ import PlayReel from "@/Homepage/Works/PlayReel";
 import Spread from "@/Homepage/Works/Spread/Spread";
 import Work1 from "@/Homepage/Works/Work1";
 import Work2 from "@/Homepage/Works/Work2";
+import Work3 from "@/Homepage/Works/Work3";
 import Work4 from "@/Homepage/Works/Work4";
 import React from "react";
 
@@ -13,7 +14,8 @@ function Works() {
       <Work1 />
       <PlayReel />
       <Work2 />
-      <Work />
+      {/* <Work /> */}
+      <Work3 />
       <Work4 />
       <Spread />
       <Footer />
