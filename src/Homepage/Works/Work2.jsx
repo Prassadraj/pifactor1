f9104b033f6315488274976d8641e1c865842c37
Prassadraj <prassadpr10@gmail.com>
2024-09-23 10:14:46 +0530

    wrong

--- src/Homepage/Works/Work2.jsx
+++ src/Homepage/Works/Work2.jsx
@@ -4,11 +4,7 @@ import Image from "next/image";
 import { Montserrat } from "@next/font/google";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/all";
-import Lenis from "@studio-freight/lenis";
 import Picture1 from "../../../public/images/1.jpeg";
-import Picture2 from "../../../public/images/2.jpeg";
-import Picture3 from "../../../public/images/3.jpg";
-import Picture4 from "../../../public/images/4.jpg";
 import Picture5 from "../../app/images/window.png";
 import Picture6 from "../../../public/images/6.jpg";
 import Picture7 from "../../../public/images/7.jpeg";
@@ -24,87 +20,95 @@ function Work2() {
   const containerRef = useRef(null);
 
   useEffect(() => {
-    // Setup GSAP animations with ScrollTrigger
-    // 2D animation text
-    gsap.fromTo(
-      ".text p",
-      {
-        y: 150,
-      },
-      {
-        y: 0,
-        scrollTrigger: ".text",
-        duration: 0.5,
-        ease: "power3.out",
-        delay: 0.2,
-        stagger: 0.1,
-      }
-    );
+    // Guard to ensure the document is only accessed on the client side
+    if (typeof window !== "undefined") {
+      // Setup GSAP animations with ScrollTrigger
+      gsap.fromTo(
+        ".text p",
+        {
+          y: 150,
+        },
+        {
+          y: 0,
+          scrollTrigger: ".text",
+          duration: 0.5,
+          ease: "power3.out",
+          delay: 0.2,
+          stagger: 0.1,
+        }
+      );
 
-    gsap.to(".animations1", {
-      height: "50vh",
-      width: "100%",
-      ease: "power3.inOut",
-      duration: 1,
-      scrollTrigger: {
-        trigger: ".animations1",
-        start: "top 80%",
-        end: "bottom 80%",
-        scrub: 2,
-      },
-    });
+      gsap.to(".animations1", {
+        height: "50vh",
+        width: "100%",
+        ease: "power3.inOut",
+        duration: 1,
+        scrollTrigger: {
+          trigger: ".animations1",
+          start: "top 80%",
+          end: "bottom 80%",
+          markers: true,
+          scrub: 2,
+        },
+      });
 
-    gsap.to(".animations2", {
-      height: "50vh",
-      width: "100%",
-      ease: "power3.inOut",
-      duration: 1,
-      scrollTrigger: {
-        trigger: ".animations2",
-        start: "top 80%",
-        end: "bottom 80%",
-        scrub: 2,
-      },
-    });
+      gsap.to(".animations2", {
+        height: "50vh",
+        width: "100%",
+        ease: "power3.inOut",
+        duration: 1,
+        scrollTrigger: {
+          trigger: ".animations2",
+          start: "top 80%",
+          end: "bottom 80%",
+          scrub: 2,
+        },
+      });
 
-    gsap.to(".animations3", {
-      height: "50vh",
-      width: "100%",
-      ease: "power3.inOut",
-      duration: 1,
-      scrollTrigger: {
-        trigger: ".animations3",
-        start: "top 80%",
-        end: "bottom 80%",
-        scrub: 2,
-      },
-    });
+      gsap.to(".animations3", {
+        height: "50vh",
+        width: "100%",
+        ease: "power3.inOut",
+        duration: 1,
+        scrollTrigger: {
+          trigger: ".animations3",
+          start: "top 80%",
+          end: "bottom 80%",
+          scrub: 2,
+        },
+      });
 
-    gsap.to(".animations4", {
-      height: "50vh",
-      width: "100%",
-      ease: "power3.inOut",
-      duration: 1,
-      scrollTrigger: {
-        trigger: ".animations4",
-        start: "top 80%",
-        end: "bottom 80%",
-        scrub: 2,
-      },
-    });
+      gsap.to(".animations4", {
+        height: "50vh",
+        width: "100%",
+        ease: "power3.inOut",
+        duration: 1,
+        scrollTrigger: {
+          trigger: ".animations4",
+          start: "top 80%",
+          end: "bottom 80%",
+          scrub: 2,
+        },
+      });
 
-    // Cleanup on component unmount
-    return () => {
-      ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
-    };
+      // Cleanup on component unmount
+      return () => {
+        ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+      };
+    }
   }, []);
 
-  const pictures = [Picture1, Picture5, Picture6, Picture7];
+  const pictures = [
+    { src: Picture1, width: 800, height: 600 },
+    { src: Picture5, width: 800, height: 600 },
+    { src: Picture6, width: 800, height: 600 },
+    { src: Picture7, width: 800, height: 600 },
+  ];
 
   return (
     <div
       ref={containerRef}
-      className="laptop:px-20 px-2 laptop:py-10 tablet:px-10 tablet:py-10 w-full h-[240vh] "
+      className="laptop:px-20 px-2 laptop:py-10 tablet:px-10 tablet:py-10 w-full h-[240vh]"
     >
       <div
         className="text flex overflow-hidden"
@@ -119,16 +123,13 @@ function Work2() {
           </p>
         ))}
       </div>
-      <p
-        className={`${montserrat.className} tablet:text-2xl laptop:text-3xl font-thin`}
-      ></p>
       <div className="hidden h-full w-full tablet:gap-5 laptop:gap-10 tablet:flex flex-col mt-4">
-        {Array.from({ length: 4 }, (_, i) => (
+        {pictures.map((picture, i) => (
           <div
             key={i}
             className={`animations${
               i + 1
-            } tablet:flex items-center  tablet:h-[25vh] tablet:w-[50%] gap-5 ${
+            } tablet:flex items-center tablet:h-[25vh] tablet:w-[50%] gap-5 ${
               i === 2 || i === 3 ? "rounded-md" : ""
             }`}
           >
@@ -143,11 +144,11 @@ function Work2() {
                 Dolores, aut eum a, vero, quia.
               </p>
             </div>
-            <div className="img-2d h-full w-full  rounded-md">
+            <div className="img-2d h-full w-full rounded-md">
               <Image
-                src={pictures[i].src}
-                width={pictures[i].width}
-                height={pictures[i].height}
+                src={picture.src}
+                width={picture.width}
+                height={picture.height}
                 className="object-cover w-full h-full"
                 alt="Window animation preview"
               />
@@ -155,8 +156,8 @@ function Work2() {
           </div>
         ))}
       </div>
-      <div className="grid grid-cols-1 tablet:hidden  gap-3 w-full h-full">
-        {Array.from({ length: 4 }, (_, i) => (
+      <div className="grid grid-cols-1 tablet:hidden gap-3 w-full h-full">
+        {pictures.map((picture, i) => (
           <div key={i} className="space-y-2">
             <div>
               <p className={`${montserrat.className} text-lg font-medium`}>
@@ -167,10 +168,10 @@ function Work2() {
                 Dolores, aut eum a, vero, quia.
               </p>
             </div>
-            <div className="h-60  w-full rounded-md overflow-hidden">
+            <div className="h-60 w-full rounded-md overflow-hidden">
               <Image
-                src={pictures[i].src}
-                width={pictures[i].width}
+                src={picture.src}
+                width={picture.width}
                 height={500}
                 className="object-cover w-full h-full"
                 alt="Window animation preview"
